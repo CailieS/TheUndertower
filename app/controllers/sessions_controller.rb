03045f45bb13d6c36480862610b2dd47b2e9715c
Cailie.Skelton <CSkelton707@gmail.com>
2020-12-29 22:17:23 -0500

    finish the login and out forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,14 @@ def new
     @user = User.new
 end
 
+def create
+    @user = User.find_by(name: params[:user][:name], email: params[:user][:email])
+    if @user && @user.authenticate(password: params[:user][:password])
+        sessions[:user_id] = @user.id
+        redirect_to user_path(@user)
+    else
+       render :new 
+    end
+end
+
 end
