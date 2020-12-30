b7871cdfa11624a699581863021cafc998aad5f2
Cailie.Skelton <CSkelton707@gmail.com>
2020-12-30 14:22:52 -0500

    create some strong params and flow with the review create feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,19 @@ class ReviewsController < ApplicationController
     def new
        @review = Review.new 
     end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            redirect_to review_path
+        else
+            redirect_to reviews_new_path
+        end
+    end
+
+    private
+    
+    def review_params
+        params.require(:review).permit(:title, :review)
+    end
 end
