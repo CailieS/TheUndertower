ca9cc668d36e2050a4400aaa81451c6698b19fdc
Cailie.Skelton <CSkelton707@gmail.com>
2021-01-01 14:33:41 -0500

    Start installing omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    def home
+        redirect_to login_path
+    end
 
-def home
-    redirect_to login_path
-end
-
+    def current_user
+        @user = User.find_by(id: session[:user_id])
+    end
 end
